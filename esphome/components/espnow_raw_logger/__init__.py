f59e7c6a310d15dd0c266c4c74a967a39baa939a
marshal08 <lubomir.beluch@gmail.com>
2025-07-11 12:55:13 +0200

    Update __init__.py

--- esphome/components/espnow_raw_logger/__init__.py
+++ esphome/components/espnow_raw_logger/__init__.py
@@ -1,9 +1,10 @@
 import esphome.codegen as cg
+import esphome.config_validation as cv  # ✅ This is where Schema lives
 
 espnow_ns = cg.esphome_ns.namespace("espnow_raw_logger")
 ESPNowRawLogger = espnow_ns.class_("ESPNowRawLogger", cg.Component)
 
-CONFIG_SCHEMA = cg.Schema({})
+CONFIG_SCHEMA = cv.Schema({})  # ✅ Use cv.Schema, not cg.Schema
 
 async def to_code(config):
     var = cg.new_Pvariable(config.get("id", "espnow_raw_logger"))
