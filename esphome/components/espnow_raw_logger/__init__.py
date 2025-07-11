de7f6dac1d84ac3d7fa4ec7d854b8019eb46fa4b
marshal08 <lubomir.beluch@gmail.com>
2025-07-11 12:40:30 +0200

    Update __init__.py

--- esphome/components/espnow_raw_logger/__init__.py
+++ esphome/components/espnow_raw_logger/__init__.py
@@ -3,11 +3,8 @@ import esphome.codegen as cg
 espnow_ns = cg.esphome_ns.namespace("espnow_raw_logger")
 ESPNowRawLogger = espnow_ns.class_("ESPNowRawLogger", cg.Component)
 
-CONFIG_SCHEMA = cg.Schema({}).extend({})
+CONFIG_SCHEMA = cg.Schema({})
 
 async def to_code(config):
-    cg.add(ESPNowRawLogger())
-
-# 👇 Register the YAML block with the component class
-espnow_raw_logger = ESPNowRawLogger()
-cg.register_component(espnow_raw_logger)
+    var = cg.new_Pvariable("espnow_raw_logger", ESPNowRawLogger())
+    cg.add(var)
