b57eb971616f2b2c9b8a4c25dd4ce6c58e6f2bd3
marshal08 <lubomir.beluch@gmail.com>
2025-07-11 13:58:26 +0200

    Update __init__.py

--- esphome/components/espnow_raw_logger/__init__.py
+++ esphome/components/espnow_raw_logger/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 
-espnow_raw_logger_ns = cg.new_namespace("espnow_raw_logger")
+espnow_raw_logger_ns = cg.esphome_ns.namespace("espnow_raw_logger")
 ESPNowRawLogger = espnow_raw_logger_ns.class_("ESPNowRawLogger", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({}).extend(cv.COMPONENT_SCHEMA)
