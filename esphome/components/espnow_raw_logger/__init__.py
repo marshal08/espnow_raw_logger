a10e330c6ba0f51779fef9f4c9bb015bae54ba69
marshal08 <lubomir.beluch@gmail.com>
2025-07-11 13:52:37 +0200

    Update __init__.py

--- esphome/components/espnow_raw_logger/__init__.py
+++ esphome/components/espnow_raw_logger/__init__.py
@@ -1,17 +1,7 @@
 import esphome.codegen as cg
-import esphome.config_validation as cv
-from esphome.const import CONF_ID
-from esphome.core import coroutine_with_priority
 
-espnow_raw_logger_ns = cg.esphome_ns.namespace("espnow_raw_logger")
-ESPNowRawLoggerComponent = espnow_raw_logger_ns.class_("ESPNowRawLoggerComponent", cg.Component)
+espnow_raw_logger_ns = cg.new_namespace("espnow_raw_logger")
+ESPNowRawLogger = espnow_raw_logger_ns.class_("ESPNowRawLogger", cg.Component)
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(ESPNowRawLoggerComponent),
-})
-
-
-@coroutine_with_priority(1.0)
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
+def to_code(config):
+    yield ESPNowRawLogger().init()
